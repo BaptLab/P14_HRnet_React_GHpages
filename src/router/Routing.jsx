ce679401fbeca8f2f3bb4fab4228b706542a44e8
Baptiste LABAUNE <baptiste.labaune@gmail.com>
2023-09-25 13:31:45 +0200

    package.json modif

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -1,9 +1,18 @@
+import React from "react";
 import { Routes, Route, HashRouter } from "react-router-dom";
 
 import Homepage from "../pages/Homepage";
 import Error404 from "../pages/Error404";
 import Employees from "../pages/Employees";
 
+/**
+ * Routing Component
+ *
+ * This component sets up the application's routing using React Router.
+ * It defines the routes and the components to render for each route.
+ *
+ * @returns {JSX.Element} The Routing component JSX.
+ */
 const Routing = () => {
   return (
     <HashRouter>
